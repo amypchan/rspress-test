55d21e487b4b23fbbd47a14fb28552a952c2d8f7
Amy Chan <amy@datacoves.com>
2025-08-21 11:09:08 -0700

    Rspress site test commit

---
+++ docs/_navbar_datacoves.md
@@ -0,0 +1,7 @@
+- <a target="_blank" href="https://datacoves.com">datacoves.com</a>
+
+- <a target="_blank" href="https://datacoves.com/learning-resources">Learning Resources</a>
+
+- <a target="_blank" href="https://github.com/datacoves">Git repos</a>
+
+- <a target="_blank" href="https://docs.getdbt.com">dbt docs</a>
---
+++ rspress.config.ts
@@ -0,0 +1,21 @@
+import * as path from 'node:path';
+import { defineConfig } from 'rspress/config';
+
+export default defineConfig({
+  root: path.join(__dirname, 'docs'),
+  title: 'Datacoves Site Test',
+  icon: '/datacoves-logo.svg',
+  logo: {
+    light: '/datacoves-logo.svg',
+    dark: '/rspress-dark-logo.png',
+  },
+  themeConfig: {
+    socialLinks: [
+      {
+        icon: 'github',
+        mode: 'link',
+        content: 'https://github.com/web-infra-dev/rspress',
+      },
+    ],
+  },
+});
